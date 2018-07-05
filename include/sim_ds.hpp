9b4676f100c8357e0f98f4585425bf264aae35af
Takuma Matsumoto <matsumoto@number-8.co.jp>
2018-07-05 15:53:44 +0900

    chage include dir constitution

---
+++ include/sim_ds.hpp
@@ -0,0 +1,19 @@
+//
+//  sim_ds.hpp
+//  BitVector_test
+//
+//  Created by 松本拓真 on 2018/07/05.
+//
+
+#ifndef sim_ds_hpp
+#define sim_ds_hpp
+
+#include "sim_ds/BitVector.hpp"
+#include "sim_ds/MultiBitVector.hpp"
+#include "sim_ds/DACs.hpp"
+#include "sim_ds/SACs.hpp"
+#include "sim_ds/Vector.hpp"
+#include "sim_ds/WaveletTree.hpp"
+#include "sim_ds/SuffixArray.hpp"
+
+#endif /* sim_ds_hpp */
