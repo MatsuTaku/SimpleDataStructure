454c1a50c8cd3ca66b353e74433ef577b5c11cf1
Takuma Matsumoto <matsumoto@number-8.co.jp>
2018-07-05 16:01:58 +0900

    re

--- SimDS.build/Debug/ALL_BUILD.build/Script-DA37AABE66B14DEE9FBAC3F8.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/takumamatsumoto/Labolatory/SimDS -f /Users/takumamatsumoto/Labolatory/SimDS/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
--- SimDS.build/Debug/ZERO_CHECK.build/Script-94E67CF1A3774196B454FCBE.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/takumamatsumoto/Labolatory/SimDS -f /Users/takumamatsumoto/Labolatory/SimDS/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
