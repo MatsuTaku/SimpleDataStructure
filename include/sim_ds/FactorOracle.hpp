15d111e978d0194fa9174bb613f26d283ab91ef9
MatsuTaku <tkm.matsumoto@gmail.com>
2019-02-26 03:35:19 +0900

    Adjust FactorOracle for running on large-scale corpus.

--- include/sim_ds/FactorOracle.hpp
+++ include/sim_ds/FactorOracle.hpp
@@ -18,22 +18,27 @@ private:
     using id_type = uint32_t;
     
     const size_t kBlockSize = 0x100;
-    const id_type kEmptyValue = std::numeric_limits<id_type>::max();
     
     std::string check_;
     std::vector<id_type> base_;
     std::vector<id_type> next_;
     
+protected:
+    const id_type kEmptyValue = std::numeric_limits<id_type>::max();
+    
 public:
-    FactorOracleBaseCTAFO(const std::string& text) : check_(text), base_(text.size() + 1, kEmptyValue) {
-        size_t empty_trans_header = 0;
+    FactorOracleBaseCTAFO(const std::string& text) : check_(text) {
+        base_.assign(text.size() + 1, kEmptyValue);
+        size_t empty_trans_front = 0;
         BitVector used_state;
         BitVector used_trans;
+        std::vector<id_type> prev;
         
         auto resize = [&](size_t size) {
             next_.resize(size);
             used_state.resize(size);
             used_trans.resize(size);
+            prev.resize(size);
         };
         
         auto expand_block = [&] {
@@ -41,31 +46,32 @@ public:
             resize(next_.size() + kBlockSize);
             size_t end = next_.size();
             // empty-element linking
-            for (long i = begin - 1; i >= 0; i--) {
-                if (not used_trans[i]) {
-                    next_[i] = begin;
-                    break;
-                }
+            if (empty_trans_front != 0 or not used_trans[0]) {
+                auto old_back = prev[empty_trans_front];
+                prev[begin] = old_back;
+                next_[old_back] = begin;
+                prev[empty_trans_front] = end - 1;
+            } else {
+                prev[begin] = end - 1;
+                empty_trans_front = begin;
             }
-            for (size_t i = begin; i < end - 1; i++) {
-                next_[i] = (i + 1) % next_.size();
+            next_[begin] = begin + 1;
+            for (size_t i = begin + 1; i < end - 1; i++) {
+                next_[i] = i + 1;
+                prev[i] = i - 1;
             }
-            for (size_t i = 0; i < begin; i++) {
-                if (not used_trans[i]) {
-                    next_[end - 1] = i;
-                    break;
-                }
-            }
-            if (empty_trans_header == 0)
-                empty_trans_header = begin;
+            next_[end - 1] = empty_trans_front;
+            prev[end - 1] = end - 2;
         };
         
         auto get_transes = [&](size_t state) -> std::vector<id_type> {
             auto b = base(state);
+            if (b == kEmptyValue)
+                return {};
             std::vector<id_type> transes;
-            for (id_type c = 0; c <= 0xff; c++) {
+            for (size_t c = 1; c <= 0xff; c++) {
                 auto n = next(b ^ c);
-                if (check(n) == c) {
+                if (used_trans[b ^ c] and n > 0 and check(n) == c) {
                     transes.push_back(n);
                 }
             }
@@ -73,33 +79,35 @@ public:
         };
         
         auto find_base = [&](std::vector<id_type> transes) -> size_t {
-            for (auto index = empty_trans_header;;) {
+            for (auto index = empty_trans_front;; index = next(index)) {
+                assert(not used_trans[index]);
                 auto b = index ^ check(transes.front());
-                bool skip = used_state[b];
-                if (not skip) {
+                if (not used_state[b]) {
+                    bool skip = false;
                     for (auto t : transes) {
                         if (used_trans[b ^ check(t)]) {
                             skip = true;
                             break;
                         }
                     }
+                    if (not skip)
+                        return b;
                 }
-                if (not skip)
-                    return b;
-                if (next(index) <= empty_trans_header) {
+                if (next(index) == empty_trans_front) {
                     expand_block();
                 }
-                if (used_state[b])
-                    index++;
-                else
-                    index = next(index);
             }
             return 0;
         };
         
         auto set_next = [&](size_t index, id_type x) {
-            if (empty_trans_header == index) {
-                empty_trans_header = next_[index];
+            assert(not used_trans[index]);
+            if (next(index) != kEmptyValue) {
+                if (empty_trans_front == index) {
+                    empty_trans_front = next(index) == empty_trans_front ? 0 : next(index);
+                }
+                next_[prev[index]] = next(index);
+                prev[next(index)] = prev[index];
             }
             used_trans[index] = true;
             next_[index] = x;
@@ -116,18 +124,22 @@ public:
             } else if (used_trans[base(state) ^ c]) {
                 auto transes = get_transes(state);
                 transes.push_back(next_state);
-                auto new_b = find_base(transes);
                 auto old_b = base(state);
+                auto new_b = find_base(transes);
                 used_state[old_b] = false;
                 used_state[new_b] = true;
                 base_[state] = new_b;
-                for (auto t : transes) {
+                // Move existing transes
+                for (size_t i = 0; i < transes.size() - 1; i++) {
+                    auto t = transes[i];
                     auto old_trans = old_b ^ check(t);
-                    auto new_trans = new_b ^ check(t);
-                    next_[old_trans] = 0;
+                    assert(used_trans[old_trans]);
+                    next_[old_trans] = kEmptyValue;
                     used_trans[old_trans] = false;
-                    set_next(new_trans, t);
+                    set_next(new_b ^ check(t), t);
                 }
+                // Insert new trans
+                set_next(new_b ^ check(next_state), next_state);
             } else {
                 auto trans = base(state) ^ c;
                 set_next(trans, next_state);
@@ -142,10 +154,13 @@ public:
             if (b == kEmptyValue)
                 return 0;
             auto trans = b ^ check(n);
-            if (check(next(trans)) != check(n)) {
+            if (not used_trans[trans])
+                return 0;
+            auto next_state = next(trans);
+            if (next_state == 0 or check(next_state) != check(n)) {
                 return 0;
             }
-            return next(trans);
+            return next_state;
         };
         
         while (next_.size() < text.size() + 1) {
@@ -153,24 +168,14 @@ public:
         }
         set_next(0, 0);
         std::vector<id_type> lrs(text.size() + 1);
-        for (size_t i = 0; i < text.size() + 1; i++) {
+        for (size_t i = 0; i < text.size(); i++) {
             lrs[0] = i + 1;
-            if (lrs[i] == i + 1) {
-                lrs[i + 1] = 0;
-            } else {
-                if (lrs[i] == 0) {
-                    lrs[i + 1] = translate(0, i + 1);
-                } else {
-                    lrs[i + 1] = translate(lrs[i], i + 1);
-                    if (lrs[i + 1] == 0)
-                        lrs[i + 1] = translate(0, i + 1);
-                }
-            }
             size_t k = lrs[i];
             while (k < i + 1 and translate(k, i + 1) == 0) {
                 insert_trans(k, i + 1);
                 k = lrs[k];
             }
+            lrs[i + 1] = k == i + 1 ? 0 : translate(k, i + 1);
         }
     }
     
@@ -183,17 +188,24 @@ public:
 };
 
 class FactorOracle : FactorOracleBaseCTAFO {
+private:
+    using Base = FactorOracleBaseCTAFO;
+    
 public:
-    FactorOracle(const std::string& text) : FactorOracleBaseCTAFO(text) {}
+    FactorOracle(const std::string& text) : Base(text) {}
     
     bool accept(std::string_view str) const {
         size_t state = 0;
         for (uint8_t c : str) {
-            if (check(state + 1) == c) {
+            if (Base::check(state + 1) == c) {
                 state++;
             } else {
-                auto next_state = next(base(state) ^ c);
-                if (next_state <= state or check(next_state) != c) {
+                auto b = Base::base(state);
+                if (b == Base::kEmptyValue) {
+                    return false;
+                }
+                auto next_state = Base::next(b ^ c);
+                if (next_state <= state or Base::check(next_state) != c) {
                     return false;
                 }
                 state = next_state;
--- test/src/FactorOracle_test.cpp
+++ test/src/FactorOracle_test.cpp
@@ -18,12 +18,26 @@ TEST(FactorOracleTest, sample) {
     EXPECT_TRUE(fo.accept("aba"));
 }
 
-TEST(FactorOracleTest, full_exploration) {
+TEST(FactorOracleTest, full_exploration_small) {
     std::string_view text("abracatabra");
     sim_ds::FactorOracle fo("abracatabra");
     for (int i = 0; i < text.size(); i++) {
         for (int j = 1; j <= text.size(); j++) {
             EXPECT_TRUE(fo.accept(text.substr(i, j - i)));
         }
+    }
+}
+
+TEST(FactorOracleTest, full_exploration_large) {
+    std::string text;
+    const size_t SIZE = 0xFFFF;
+    const std::string alphabets = "abc";
+    for (size_t i = 0; i < SIZE; i++) {
+        text.push_back(alphabets[rand() % 3]);
+    }
+    sim_ds::FactorOracle fo(text);
+    std::string_view text_view(text);
+    for (int i = 0; i < SIZE; i++) {
+        EXPECT_TRUE(fo.accept(text_view.substr(i)));
     }
 }
